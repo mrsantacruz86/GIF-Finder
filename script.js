70d872a217c1f4760817862b1667878e1a1a4315
mrsantacruz86 <adcarballido@ymail.com>
2018-03-06 23:28:30 -0500

    displaying the gifs

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ var animalArray = ["dog","cat", "rabbit", "hamster", "goldfish", "bird"];
 
 function apiRequest(topic, limit) {
     // API-KEY
-    myKey = "dc6zaTOxFJmzC"
+    var myKey = "dc6zaTOxFJmzC"
     // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    topic + "&api_key=" + myKey + "&limit=" + limit;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + myKey + "&limit=" + limit;
+    // console.log(queryURL);
+    var collection;
     // Performing an AJAX request with the queryURL
-    $.ajax({
+    return $.ajax({
         url: queryURL,
         method: "GET"
     })
     // After data comes back from the request
-    .then(function (response) {
-        console.log(queryURL);
-        console.log(response);
-        return respose;
-    });
+    // .then(function (response) {
+    //     console.log(response.data);
+    //     return response.data;
+    // });
     
 }
 
@@ -29,54 +29,49 @@ function createButton(animalName){
     $button.text(animalName);
     $button.appendTo('#animal-buttons');
 }
-function refreshButtons(list){
-    $('#animal-buttons').empty();
-    for(var i = 1; i < list.length; i++){
+function addButton(list){
         createButton(list[i]);
-    }
 }
 
-
 $(document).ready(function(){
-    refreshButtons(animalArray);
+    addButton(animalArray);
     // Adding click event listen listener to all buttons
-    $(".animal-button").click(function () {
+    $(".animal-btn").click(function () {
+        $("#gifs-appear-here").empty();
         // Grabbing and storing the data-animal property value from the button
         var animal = $(this).attr("data-animal");
-    
         // Constructing a queryURL using the animal name
         // storing the data from the AJAX request in the results variable
-        var results = response.data;
-
-        // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
-
-            // Creating and storing a div tag
-            var animalDiv = $("<div>");
-
-            // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + results[i].rating);
-
-            // Creating and storing an image tag
-            var animalImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            animalImage.attr("src", results[i].images.fixed_height.url);
-
-            // Appending the paragraph and image tag to the animalDiv
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
-
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-            $("#gifs-appear-here").prepend(animalDiv);
-        }
+        var results = apiRequest(animal,10).then(
+            function(response) {
+                console.log("llego el pollo",response)
+                // Looping through each result item
+                for (var i = 0; i < response.data.length; i++) {
+                    // Creating and storing a div tag
+                    var animalDiv = $("<div>");
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + response.data[i].rating);
+                    // Creating and storing an image tag
+                    var animalImage = $("<img>");
+                    // Setting the src attribute of the image to a property pulled off the result item
+                    animalImage.attr("src", response.data[i].images.fixed_height.url);
+                    // Appending the paragraph and image tag to the animalDiv
+                    animalDiv.append(p);
+                    animalDiv.append(animalImage);
+                    // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+                    $("#gifs-appear-here").prepend(animalDiv);
+                }
+            });
     });
 
     $("#add-btn").click(function (event) {
         event.preventDefault();
         var text = $('#search').val()
-        $('#search').val("");
-        animalArray.push(text);
-        console.log(animalArray);
-        refreshButtons(animalArray);
+        if(text != ""){
+            $('#search').val("");
+            animalArray.push(text);
+            console.log(animalArray);
+            addButton(animalArray);
+        }
     });
 })
